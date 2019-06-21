1b8e2d8fc921de05fa0767bfb3308afc58269939
ofrahaza <fjee@mail.ru>
2019-06-21 11:27:08 +0300

    2307, 2308, 2309, 2310, 2311 solved. 2401, 2402, 2403, 2404, 2405, 2406, 2407, 2408, 2409, 2410, 2411, 2412 solved. 2501, 2502, 2503, 2504, 2505, 2506 solved.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/User.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2309.vo;
+
+import com.javarush.task.task23.task2309.AbstractDbSelectExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends NamedItem {
+
+}
