3890d642cde63555e7ba74be19782c99d52759aa
ofrahaza <fjee@mail.ru>
2019-08-13 18:01:00 +0300

    3008

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -5,7 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class Server {
     public static void main(String args[]) throws IOException {
@@ -49,9 +48,65 @@ public class Server {
 
         private Socket socket;
 
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            Message message = new Message(MessageType.NAME_REQUEST);
+            Message messageReceive;
+            String name = "";
+
+            while(true) {
+                connection.send(message);
+                messageReceive = connection.receive();
+                if (messageReceive.getType().equals(MessageType.USER_NAME) && !messageReceive.getData().equals("") && !connectionMap.containsKey(messageReceive.getData())) {
+                        connectionMap.put(messageReceive.getData(), connection);
+                        connection.send(new Message(MessageType.NAME_ACCEPTED));
+                        name = messageReceive.getData();
+                        break;
+                }
+            }
+            return name;
+        }
+
+
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (Map.Entry<String, Connection> e : connectionMap.entrySet()) {
+                if (!e.getKey().equals(userName)) {
+                    connection.send(new Message(MessageType.USER_ADDED, e.getKey()));
+                }
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+
+            while (true) {
+                Message messageReceive = connection.receive();
+                if (messageReceive.getType()==MessageType.TEXT) {
+                    sendBroadcastMessage(new Message(MessageType.TEXT, String.format("%s: " + messageReceive.getData(), userName)));
+                } else {
+                    ConsoleHelper.writeMessage("Text error");
+                }
+            }
+        }
+
         @Override
         public void run() {
-            super.run();
+            ConsoleHelper.writeMessage("Connection established with " + socket.getRemoteSocketAddress());
+            String s = "";
+            try {
+                Connection connection = new Connection(socket);
+                s = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, s));
+                notifyUsers(connection,s);
+                serverMainLoop(connection,s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                connectionMap.remove(s);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, s));
+            }
+
         }
 
     }
